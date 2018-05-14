9197f0a224cf04942e5bf217435b45dae3eb66b4
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-14 15:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,13 +19,5 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
   
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
-    
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-    }
-  }
 }
